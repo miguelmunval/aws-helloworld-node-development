ed15883dbfa8e4606ebe0bbbc0e2d00fb7f14248
Miguel Ángel Muñoz Valenzuela <91052453+miguelmunval@users.noreply.github.com>
2023-03-09 10:41:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
-const message = "Hola Mundo";
+const message = "Hola soy Miguel";
 const port = 8080;
 
 const server = http.createServer((req,res) => {
